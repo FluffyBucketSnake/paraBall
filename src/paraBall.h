09ce979d641db407572b098087bf5e067ad46d29
Bruno Henrique Glowaski Morais <fluffybucketsnake@localhost.localdomain>
2018-12-01 22:51:36 -0300

    Unmark paraBall class's function as virtual

--- src/paraBall.h
+++ src/paraBall.h
@@ -5,9 +5,9 @@
 class paraBall : public Game
 {
     public:
-        virtual void Init();
-        virtual void Update(int delta);
-        virtual void Render(int delta);
-        virtual void Load();
-        virtual void Unload();
+        void Init();
+        void Update(int delta);
+        void Render(int delta);
+        void Load();
+        void Unload();
 };
